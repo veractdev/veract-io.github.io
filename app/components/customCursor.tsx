5eb707ebb3f6bda3ca49a841f45894dd82652dec
kiran <kiranmavi5502@gmail.com>
2023-10-24 15:44:15 +0530

    portfolio inner pages changes

--- app/components/customCursor.tsx
+++ app/components/customCursor.tsx
@@ -2,32 +2,37 @@
 import React, { useEffect } from 'react';
 import '../styles/cursor.css';
 
-export default function CustomCursor() {
-  useEffect(() => {
-    const cursor = document.querySelector('.cursor');
-    if (cursor) {
-      const handleMouseMove = (e: { pageY: number; pageX: number; }) => {
-        cursor.setAttribute("style", `top: ${e.pageY - 10}px; left: ${e.pageX - 10}px;`);
-      };
-
-
-      const handleClick = () => {
-        cursor.classList.add("expand");
-
-        setTimeout(() => {
-          cursor.classList.remove("expand");
-        }, 500);
-      };
-
-      document.addEventListener('mousemove', handleMouseMove);
-      document.addEventListener('click', handleClick);
-      return () => {
-        document.removeEventListener('mousemove', handleMouseMove);
-        document.removeEventListener('click', handleClick);
-      };
-    };
-  }, []);
-  return (
-    <div className='cursor'></div>
-  )
+export default function CustomCursor(){
+    useEffect(() => {
+        const cursor = document.querySelector('.cursor ');
+        // const cursor1= document.querySelector('.cursor1 ')
+        if (cursor ) {
+            const handleMouseMove = (e: { pageY: number; pageX: number; }) => {
+              cursor.setAttribute("style", `top: ${e.pageY - 10}px; left: ${e.pageX - 10}px;`);
+            };
+        
+    
+        const handleClick = () => {
+          cursor.classList.add("expand");
+          // cursor1.classList.add("expand");
+    
+          setTimeout(() => {
+            cursor.classList.remove("expand");
+            // cursor1.classList.remove("expand");
+          }, 500);
+        };
+    
+        document.addEventListener('mousemove', handleMouseMove);
+        document.addEventListener('click', handleClick);
+        return () => {
+            document.removeEventListener('mousemove', handleMouseMove);
+            document.removeEventListener('click', handleClick);
+          };
+ }; }, []);
+        return(
+            <div>
+              <div className='cursor '></div>
+              {/* <div className='cursor1 '></div> */}
+            </div>
+            )
 }
---
+++ app/components/dashboardPortfolio.tsx
@@ -0,0 +1,35 @@
+import '../styles/styles.css';
+
+export default function DashboardDetails(){
+    return(
+        <div className='dashboard'>
+            <div className='pt-24 '><img src="veractLogo.jpg " className='w-40'/></div>
+            <div className='menuDetails'>
+                <div className='menuRowDetails'>
+                    <div>icon</div>
+                    <div>Manufacturing</div>
+                </div>
+                <div className='menuRowDetails'>
+                    <div>icon</div>
+                    <div>Wellness</div>
+                </div>
+                <div className='menuRowDetails'>
+                    <div>icon</div>
+                    <div>Sales</div>
+                </div>
+                <div className='menuRowDetails'>
+                    <div>icon</div>
+                    <div>Agri tech</div>
+                </div>
+                <div className='menuRowDetails'>
+                    <div>icon</div>
+                    <div>Hr tech</div>
+                </div>
+                <div className='menuRowDetails'>
+                    <div>icon</div>
+                    <div>Fin tech</div>
+                </div>
+            </div>
+        </div>
+    )
+}
