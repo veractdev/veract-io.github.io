6fc0ff701ecbf655e50fb22cd93c3a088a3b284e
Ekesh <ekeshr2@gmail.com>
2023-11-17 19:22:16 +0530

    MobileCaseStudy carousel

--- app/MobileCaseStudy/Carousel/page.tsx
+++ app/MobileCaseStudy/Carousel/page.tsx
@@ -1,34 +1,34 @@
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+// import Slider from "react-slick";
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
 
-const HorizontalCarousel = () => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
+// const HorizontalCarousel = () => {
+//   const settings = {
+//     dots: true,
+//     infinite: true,
+//     speed: 500,
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//   };
 
-  const images = [
-    'image1.jpg',
-    'image2.jpg',
-    'image3.jpg',
-    // Add more image URLs as needed
-  ];
+//   const images = [
+//     'image1.jpg',
+//     'image2.jpg',
+//     'image3.jpg',
+//     // Add more image URLs as needed
+//   ];
 
-  return (
-    <div className="carousel">
-      <Slider {...settings}>
-        {images.map((image, index) => (
-          <div key={index} className="carousel-item">
-            <img src={image} alt={`Image ${index + 1}`} />
-          </div>
-        ))}
-      </Slider>
-    </div>
-  );
-};
+//   return (
+//     <div className="carousel">
+//       <Slider {...settings}>
+//         {images.map((image, index) => (
+//           <div key={index} className="carousel-item">
+//             <img src={image} alt={`Image ${index + 1}`} />
+//           </div>
+//         ))}
+//       </Slider>
+//     </div>
+//   );
+// };
 
-export default HorizontalCarousel;
+// export default HorizontalCarousel;
