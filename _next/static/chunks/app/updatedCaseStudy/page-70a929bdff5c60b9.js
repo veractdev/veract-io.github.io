d38f6eec593869581cd0891cd4348285d3f96f3e
veract dev <veractdev@gmail.com>
2025-04-04 17:35:00 +0530

    deploy

---
+++ _next/static/chunks/app/updatedCaseStudy/page-70a929bdff5c60b9.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[8132],{318:function(e,n,i){Promise.resolve().then(i.bind(i,3508)),Promise.resolve().then(i.bind(i,691)),Promise.resolve().then(i.t.bind(i,352,23))},352:function(){}},function(e){e.O(0,[1176,6972,5772,3508,691,2971,596,1744],function(){return e(e.s=318)}),_N_E=e.O()}]);
