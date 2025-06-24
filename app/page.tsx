64d774c7f0b68d69b32f0f4eeb41e6d035fc181a
veract dev <veractdev@gmail.com>
2025-06-24 16:56:55 +0530

    veract revamp initial code with navbar

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Navbar from './components/layout/Navbar';
+
+export default function Page() {
+  return (
+    <div className='w-screen h-screen flex flex-col bg-white'>
+      <Navbar />
+    </div>
+  );
+}
---
+++ lib/custom_data.ts
@@ -0,0 +1,27 @@
+export const navItems = [
+    {
+        id: 1,
+        name: 'Case Studies',
+        link: ''
+    },
+    {
+        id: 2,
+        name: 'Services',
+        link: ''
+    },
+    {
+        id: 3,
+        name: 'Industries',
+        link: ''
+    },
+    {
+        id: 4,
+        name: 'Company',
+        link: ''
+    },
+    {
+        id: 5,
+        name: 'Blogs',
+        link: ''
+    }
+]
