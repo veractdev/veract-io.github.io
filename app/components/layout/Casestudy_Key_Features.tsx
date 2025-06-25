127e44d2a78fcb5be01b5f4b7119b0854a5505f6
sruthi chandrasekar <sruthiamutharani@gmail.com>
2025-06-25 18:01:56 +0530

    view transition in key features

--- app/components/layout/Casestudy_Key_Features.tsx
+++ app/components/layout/Casestudy_Key_Features.tsx
@@ -38,7 +38,6 @@ const features = [
 export default function Casestudy_Key_Features(){
     return(
         <div className="lg:w-[1100px] md:w-[710px] w-full  py-[57px] flex flex-col lg:px-[30px] px-[20px] bg-white mt-[135px] geistFont mb-[10rem]">
-        {isMobile() ? (
           <motion.div
             initial={{ opacity: 0, y: 40 }}
             whileInView={{ opacity: 1, y: 0 }}
@@ -47,12 +46,8 @@ export default function Casestudy_Key_Features(){
           >
             <div className="text-left text-[#868586] font-semibold text-[1rem]">KEY FEATURES</div>
           </motion.div>
-        ) : (
-          <div className="text-left text-[#868586] font-semibold text-[1rem]">KEY FEATURES</div>
-        )}
         <div className="flex flex-col lg:w-[1040px] w-full">
           {features.map((feature, idx) =>
-            isMobile() ? (
               <motion.div
                 key={feature.number}
                 className="group relative flex lg:flex-row md:flex-col flex-col lg:items-center items-start py-[50px] lg:opacity-60 hover:opacity-100 border-b-[1px] border-[#282828] lg:gap-0 gap-[15px]"
@@ -68,19 +63,6 @@ export default function Casestudy_Key_Features(){
                 </div>
                 <div className="lg:w-[213px] lg:pl-[20px] text-[#282828] interFont text-[14px] leading-[1.4em]">{feature.desc}</div>
               </motion.div>
-            ) : (
-              <div
-                key={feature.number}
-                className="group relative flex lg:flex-row md:flex-col flex-col lg:items-center items-start py-[50px] lg:opacity-60 hover:opacity-100 border-b-[1px] border-[#282828] lg:gap-0 gap-[15px]"
-              >
-                <div className="pr-[26px] text-[#FF7A3B] text-[18px] interFont leading-[1.4em]">{feature.number}</div>
-                <div className="lg:w-[488px] w-full ls:text-[40px] text-[28px] lg:mr-[270px] lg:pr-[1rem] text-[#0D0D0D] font-medium leading-[1.2em]">{feature.title}</div>
-                <div className="absolute right-[14rem] opacity-0 group-hover:opacity-100 transition-all duration-500 group-hover:-rotate-15 p-[20px] backdrop-blur-[10px]">
-                  <img src={feature.img} className="w-[280px] h-[305px]"/>
-                </div>
-                <div className="lg:w-[213px] lg:pl-[20px] text-[#282828] interFont text-[14px] leading-[1.4em]">{feature.desc}</div>
-              </div>
-            )
           )}
         </div>
       </div>
