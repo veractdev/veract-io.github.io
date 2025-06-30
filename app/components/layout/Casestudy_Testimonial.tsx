2a0b47d7d41dee7131025949bd4e38266c70c78a
shiva <veractdev@gmail.com>
2025-06-30 09:34:53 +0530

    mobile view inprogress

--- app/components/layout/Casestudy_Testimonial.tsx
+++ app/components/layout/Casestudy_Testimonial.tsx
@@ -8,10 +8,11 @@ interface TestimonialProps {
   shadow?: string;
 }
 
+
 export default function Casestudy_Testimonial() {
 
   // Sample data for the testimonial
-  const testimonialData = {
+  const testimonialData : TestimonialProps = {
     testimonial: `"Veract brought our vision to life with precision. The voice-to-text AI solution exceeded expectations—intuitive, responsive, and impactful. Our users love it. A truly seamless collaboration"`,
     image : "/Images/testimonial_avatar.png",
     name: "Gwan Sik",
@@ -21,24 +22,47 @@ export default function Casestudy_Testimonial() {
   }
 
   return (
-    <div className="w-full h-[100vh] bg-black flex flex-col items-center justify-center ">
-    <img src='Images/horizantal_design.png' alt="horizantal line"/>      
-    <div className="max-w-[47.125rem] h-[18.75rem] bg-transparent flex">   
-      <div className="w-[13.56rem] relative">
-          <div className="w-full h-full rounded-[0.75rem] bg-no-repeat bg-center bg-cover" style={{ backgroundImage:`url(${testimonialData.image})`,boxShadow:testimonialData.shadow }}></div> 
-          <img src='/Images/vertical_design.png' alt="vertical line" className="absolute left-0 top-[-15%]"/>      
-          <img src='/Images/vertical_design.png' alt="vertical line" className="absolute right-0 top-[-15%]"/>   
+  <>
+  <div className="w-full min-h-screen bg-black flex flex-col items-center justify-center px-4 py-8 z-[10]">
+  <img src="Images/horizantal_design.png" alt="horizontal line" className="lg:block hidden" />
+  <div className="w-full lg:max-w-[47.125rem] bg-transparent flex flex-col lg:flex-row items-center lg:items-stretch">
+    <div className="relative w-full lg:w-[13.56rem] h-[20rem] lg:h-auto flex justify-center items-center mb-4 lg:mb-0 relaltive">
+      <img src="Images/horizantal_design.png" alt="horizontal line" className="absolute top-[8%] lg:hidden block "/>
+      <img src="Images/horizantal_design.png" alt="horizontal line" className="absolute bottom-[8%] lg:hidden block"/>
+      <div className="w-fit lg:w-full h-[100%] lg:h-[100%] flex justify-center items-center relative">
+        <img src="/Images/vertical_design.png" alt="horizontal line" className="absolute left-0 lg:hidden block"/>
+        <img src="/Images/vertical_design.png" alt="horizontal line" className="absolute right-0 lg:hidden block"/>
+        <img src={testimonialData.quotesImage} alt="quotes image" className="w-[3.07rem] h-[2.456rem] absolute bottom-[1rem] right-[-4rem] lg:hidden block"/>
+        <div className="w-[12rem] h-[16rem] lg:w-full lg:h-full rounded-[1.25rem] bg-no-repeat bg-center bg-cover"
+          style={{
+            backgroundImage: `url(${testimonialData.image})`,
+            boxShadow: testimonialData.shadow,
+          }}
+        >  
+        </div>
       </div>
-      <div className="w-[25.8rem] bg-transperent flex flex-col justify-center items-start p-[1.5rem]">
-            <div className="text-[1.438rem] tracking-[-0.01em] leading-[1.4em] text-left text-white interFont font-medium">{testimonialData.testimonial}</div>
-            <div className="mt-[1.25rem]">
-                <div className="text-left text-white text-[1rem] leading-[1.4em] font-medium">{testimonialData.name}</div>
-                <div className="text-[0.875rem] text-white/50 font-medium leading-[1.2em]">{testimonialData.designation}</div>
-            </div>
+      <img src="/Images/vertical_design.png" alt="vertical line" className="absolute left-0 top-[-12%] hidden lg:block"/>
+      <img src="/Images/vertical_design.png" alt="vertical line" className="absolute right-0 top-[-12%] hidden lg:block"/>
+    </div>
+    <div className="w-full lg:w-[25.8rem] text-center lg:text-left px-2 lg:p-[1.5rem] flex flex-col justify-center items-center lg:items-start">
+      <div className="text-[1rem] lg:text-[1.438rem] tracking-[-0.01em] leading-[1.4em] text-white interFont lg:font-medium font-[600]">
+        {testimonialData.testimonial}
       </div>
-      <div className="w-[7.765rem] flex flex-col flex-end items-center justify-end"><img src={testimonialData.quotesImage} alt="quotes image" className="mb-[2.75rem]"/></div>
+      <div className="mt-[1.25rem]">
+        <div className="text-white text-[1rem] leading-[1.4em] font-medium">
+          {testimonialData.name}
+        </div>
+        <div className="text-[0.875rem] text-white/50 font-medium leading-[1.2em]">
+          {testimonialData.designation}
+        </div>
+      </div>
     </div>
-    <img src='Images/horizantal_design.png' alt="horizantal line"/>       
+    <div className="w-full lg:w-[7.765rem] flex justify-center lg:justify-end lg:items-end mt-6 lg:mt-0 lg:mb-[2rem]">
+    <img src={testimonialData.quotesImage} alt="quotes image" className="w-[5.875rem] h-[4.688rem] hidden lg:block"/>
     </div>
+  </div>
+  <img src="Images/horizantal_design.png" alt="horizontal line" className="lg:block hidden" />
+  </div>
+  </>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,21 @@
-'use client'
-import React, { useEffect } from 'react'
-import Navbar from './components/layout/Navbar';
-import UserServices from './components/layout/UserServices';
-import Casestudy_Banner from './components/layout/Casestudy_Banner';
-import Casestudy_ShowReel from './components/layout/Casestudy_ShowReel';
-import Casestudy_Testimonial from './components/layout/Casestudy_Testimonial';
-import Casestudy_Key_Features from './components/layout/Casestudy_Key_Features';
+"use client";
+import React, { useEffect } from "react";
+import Navbar from "./components/layout/Navbar";
+import UserServices from "./components/layout/UserServices";
+import Casestudy_Banner from "./components/layout/Casestudy_Banner";
+import Casestudy_ShowReel from "./components/layout/Casestudy_ShowReel";
+import Casestudy_Testimonial from "./components/layout/Casestudy_Testimonial";
+import Casestudy_Key_Features from "./components/layout/Casestudy_Key_Features";
 
 export default function Page() {
   return (
-    <div className='w-screen flex items-center justify-center flex-col bg-black'>
+    <div className="w-screen flex items-center justify-center flex-col bg-black">
       <Navbar />
       <Casestudy_Banner />
       <Casestudy_ShowReel />
-      <Casestudy_Testimonial/>
       <UserServices />
       <Casestudy_Key_Features />
+      <Casestudy_Testimonial />
     </div>
   );
 }
