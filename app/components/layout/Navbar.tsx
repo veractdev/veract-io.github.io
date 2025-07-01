b03ed1faa79ab002488f7c72218f76226d2e9f49
veract dev <veractdev@gmail.com>
2025-07-01 14:31:12 +0530

    navbar - prettier

--- app/components/layout/Navbar.tsx
+++ app/components/layout/Navbar.tsx
@@ -5,195 +5,174 @@ import { useRouter } from "next/navigation";
 import React, { useEffect, useRef, useState } from "react";
 
 export default function Navbar() {
-  const router = useRouter();
-  const [selectedNav, setSelectedNav] = useState<number>();
-  const [isHamburgerMenu, setIsHamburgerMenu] = useState<boolean>(false);
-  const [loaded, setLoaded] = useState<boolean>(false);
-  const menuRef = useRef<HTMLDivElement | null>(null);
+    const router = useRouter();
+    const [selectedNav, setSelectedNav] = useState<number>();
+    const [isHamburgerMenu, setIsHamburgerMenu] = useState<boolean>(false);
+    const [loaded, setLoaded] = useState<boolean>(false);
+    const menuRef = useRef<HTMLDivElement | null>(null);
 
 
     useEffect(() => {
         setLoaded(true);
     }, [])
 
-  useEffect(() => {
-    const handleResize = () => {
-      if (window && typeof window !== "undefined") {
-        if (window.innerWidth >= 1024) {
-          setIsHamburgerMenu(false);
-        }
-      }
-    };
-    // Initial check
-    handleResize();
-    // Add resize listener
-    window.addEventListener("resize", handleResize);
-    // Cleanup
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
+    useEffect(() => {
+        const handleResize = () => {
+            if (window && typeof window !== "undefined") {
+                if (window.innerWidth >= 1024) {
+                    setIsHamburgerMenu(false);
+                }
+            }
+        };
+        // Initial check
+        handleResize();
+        // Add resize listener
+        window.addEventListener("resize", handleResize);
+        // Cleanup
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
 
 
-  // outside click handler to close the hamburger menu
-  // and scroll handler to close the hamburger menu on scroll
-  useEffect(() => {
-  const handleClickOutside = (event: MouseEvent) => {
-    if (
-      menuRef.current &&
-      !menuRef.current.contains(event.target as Node)
-    ) {
-      setIsHamburgerMenu(false);
-    }
-  };
+    // outside click handler to close the hamburger menu
+    // and scroll handler to close the hamburger menu on scroll
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            if (
+                menuRef.current &&
+                !menuRef.current.contains(event.target as Node)
+            ) {
+                setIsHamburgerMenu(false);
+            }
+        };
 
-  const handleScroll = () => {
-    setIsHamburgerMenu(false);
-  };
+        const handleScroll = () => {
+            setIsHamburgerMenu(false);
+        };
 
-  document.addEventListener("mousedown", handleClickOutside);
-  window.addEventListener("scroll", handleScroll);
+        document.addEventListener("mousedown", handleClickOutside);
+        window.addEventListener("scroll", handleScroll);
 
-  return () => {
-    document.removeEventListener("mousedown", handleClickOutside);
-    window.removeEventListener("scroll", handleScroll);
-  };
-}, []);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
 
 
-  return (
-    <React.Fragment>
-      {loaded && (
-        <div
-          className={`flex fixed top-0 z-[999] items-center justify-center ${
-            isMobile()
-              ? "py-[0.625rem_0.375rem]"
-              : `${
-                  isTablet()
-                    ? "py-[0.625rem_0.375rem]"
-                    : "py-[1.875rem_0.375rem]"
-                }`
-          }`}
-        >
-          <div
-            ref={menuRef}
-            className={`lg:w-[56.25rem] md:w-[40.813rem] w-[21.875rem] ${
-              isHamburgerMenu ? "h-max" : "h-[3.5rem]"
-            } rounded-[2.75rem] flex ${
-              isHamburgerMenu ? "flex-col" : "flex-row"
-            } items-center lg:justify-between bg-background backdrop-blur-[0.563rem] ${
-              isHamburgerMenu
-                ? `${
-                    isMobile() ? "py-[6.5rem_1.25rem]" : "py-[4.125rem_1.25rem]"
-                  }`
-                : "p-[0.375rem_0.438rem_0.375rem_1.25rem]"
-            } backdrop-blur-[0.563rem] transform transform-gpu transition-all duration-300 ease-in`}
-          >
-            <img
-              src="/Icons/veract-logo.png"
-              alt="veract-logo"
-              className={`
+    return (
+        <React.Fragment>
+            {loaded && (
+                <div
+                    className={`flex fixed top-0 z-[999] items-center justify-center ${isMobile()
+                        ? "py-[0.625rem_0.375rem]"
+                        : `${isTablet()
+                            ? "py-[0.625rem_0.375rem]"
+                            : "py-[1.875rem_0.375rem]"
+                        }`
+                        }`}
+                >
+                    <div
+                        ref={menuRef}
+                        className={`lg:w-[56.25rem] md:w-[40.813rem] w-[21.875rem] ${isHamburgerMenu ? "h-max" : "h-[3.5rem]"
+                            } rounded-[2.75rem] flex ${isHamburgerMenu ? "flex-col" : "flex-row"
+                            } items-center lg:justify-between bg-background backdrop-blur-[0.563rem] ${isHamburgerMenu
+                                ? `${isMobile() ? "py-[6.5rem_1.25rem]" : "py-[4.125rem_1.25rem]"
+                                }`
+                                : "p-[0.375rem_0.438rem_0.375rem_1.25rem]"
+                            } backdrop-blur-[0.563rem] transform transform-gpu transition-all duration-300 ease-in`}
+                    >
+                        <img
+                            src="/Icons/veract-logo.png"
+                            alt="veract-logo"
+                            className={`
                                 absolute left-[1.25rem] w-[9.5rem] h-[2.75rem] object-contain cursor-pointer 
                                 transition-all duration-500 ease-in 
-                                ${
-                                  isHamburgerMenu
+                                ${isHamburgerMenu
                                     ? "top-[1.875rem]"
                                     : "top-[0.25rem]"
                                 }
                                 lg:static lg:top-auto lg:left-auto
                             `}
-            />
+                        />
 
-            <div
-              className={`flex ${
-                isHamburgerMenu ? "flex-col" : "flex-row"
-              } items-center justify-center
-              ${isHamburgerMenu && "opacity-0 animate-fade-in"}`}
-            >
-              {navItems.map((item) => (
-                <div
-                  key={item.id}
-                  // first:ml-[210px]
-                  className={`${
-                    isHamburgerMenu
-                      ? ""
-                      : " max-sm:hidden max-md:hidden max-lg:hidden"
-                  } relative flex items-center justify-center group`}
-                  onClick={() => {
-                    setSelectedNav(item.id);
-                    router.push(item.link);
-                  }}
-                >
-                  <div
-                    className={`syneFont p-[0.875rem_1rem] text-[1rem] leading-[1em] 
-                                            tracking-[-0.05em] text-nowrap ${
-                                              selectedNav === item.id
+                        <div
+                            className={`flex ${isHamburgerMenu ? "flex-col" : "flex-row"} items-center justify-center ${isHamburgerMenu && "opacity-0 animate-fade-in"}`}
+                        >
+                            {navItems.map((item) => (
+                                <div
+                                    key={item.id}
+                                    // first:ml-[210px]
+                                    className={`${isHamburgerMenu
+                                        ? ""
+                                        : " max-sm:hidden max-md:hidden max-lg:hidden"
+                                        } relative flex items-center justify-center group`}
+                                    onClick={() => {
+                                        setSelectedNav(item.id);
+                                        router.push(item.link);
+                                    }}
+                                >
+                                    <div
+                                        className={`syneFont p-[0.875rem_1rem] text-[1rem] leading-[1em] 
+                                            tracking-[-0.05em] text-nowrap ${selectedNav === item.id
                                                 ? "text-[#4285F4]"
                                                 : "text-[#FFFFFF]"
                                             } group-hover:text-[#4285F4] font-semibold cursor-pointer 
                                             transition-all duration-300 rounded-[1.625rem] 
                                             group-hover:bg-[#FFFFFF1A]`}
-                  >
-                    {item.name}
-                  </div>
-                  <div
-                    className={`absolute bottom-0 h-[0.125rem] rounded-[0.688rem] w-0 ${
-                      selectedNav === item.id
-                        ? "w-[calc(100%-3.563rem)]"
-                        : "w-0"
-                    } group-hover:w-[calc(100%-3.563rem)] bg-[#4285F4] transition-all duration-300`}
-                  />
+                                    >
+                                        {item.name}
+                                    </div>
+                                    <div
+                                        className={`absolute bottom-0 h-[0.125rem] rounded-[0.688rem] w-0 ${selectedNav === item.id
+                                            ? "w-[calc(100%-3.563rem)]"
+                                            : "w-0"
+                                            } group-hover:w-[calc(100%-3.563rem)] bg-[#4285F4] transition-all duration-300`}
+                                    />
+                                </div>
+                            ))}
+                            <div
+                                className={`${isHamburgerMenu
+                                    ? `${isMobile() ? "mt-[3.875rem]" : "mt-[3.125rem]"}`
+                                    : "max-sm:hidden max-md:hidden max-lg:hidden"
+                                    } syneFont bg-white text-black font-bold leading-[1.2em] tracking-[0em] p-[0.563rem_2.063rem] rounded-[1.875rem] cursor-pointer hover:bg-[#FF7A3B] hover:text-white transition-all duration-300 ease-in-out ${!isMobile() && !isTablet() ? "ml-[.625rem]" : ""
+                                    } text-nowrap`}
+                            >
+                                Talk to us
+                            </div>
+                        </div>
+                        <div
+                            className={`min-lg:hidden fixed ${isHamburgerMenu
+                                ? "top-[1.875rem] right-[.625rem]"
+                                : "right-[.438rem]"
+                                } w-[2.75rem] h-[2.75rem] rounded-full ${isHamburgerMenu ? "bg-transparent" : "bg-[#FFFFFF1A]"
+                                } flex items-center justify-center transform transition-all duration-500 ease-in`}
+                            onClick={() => {
+                                setIsHamburgerMenu(!isHamburgerMenu);
+                            }}
+                        >
+                            <div
+                                className={`flex flex-col gap-[0.438rem] ${isHamburgerMenu ? "items-center" : "items-end"
+                                    } justify-center transition-all duration-300 ease-in`}
+                            >
+                                <div
+                                    className={`w-[1.375rem] h-[0.125rem] bg-white transition-all duration-300 ease-in ${isHamburgerMenu ? "absolute rotate-45" : ""
+                                        }`}
+                                />
+                                <div
+                                    className={`${isHamburgerMenu ? "hidden" : ""
+                                        } w-[.75rem] h-[0.125rem] bg-white`}
+                                />
+                                <div
+                                    className={`w-[1.375rem] h-[0.125rem] bg-white transition-all duration-300 ease-in ${isHamburgerMenu ? "absolute -rotate-45" : ""
+                                        }`}
+                                />
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              ))}
-              <div
-                className={`${
-                  isHamburgerMenu
-                    ? `${isMobile() ? "mt-[3.875rem]" : "mt-[3.125rem]"}`
-                    : "max-sm:hidden max-md:hidden max-lg:hidden"
-                } syneFont bg-white text-black font-bold leading-[1.2em] tracking-[0em] p-[0.563rem_2.063rem] rounded-[1.875rem] cursor-pointer hover:bg-[#FF7A3B] hover:text-white transition-all duration-300 ease-in-out ${
-                  !isMobile() && !isTablet() ? "ml-[.625rem]" : ""
-                } text-nowrap`}
-              >
-                Talk to us
-              </div>
-            </div>
-            <div
-              className={`min-lg:hidden fixed ${
-                isHamburgerMenu
-                  ? "top-[1.875rem] right-[.625rem]"
-                  : "right-[.438rem]"
-              } w-[2.75rem] h-[2.75rem] rounded-full ${
-                isHamburgerMenu ? "bg-transparent" : "bg-[#FFFFFF1A]"
-              } flex items-center justify-center transform transition-all duration-500 ease-in`}
-              onClick={() => {
-                setIsHamburgerMenu(!isHamburgerMenu);
-              }}
-            >
-              <div
-                className={`flex flex-col gap-[0.438rem] ${
-                  isHamburgerMenu ? "items-center" : "items-end"
-                } justify-center transition-all duration-300 ease-in`}
-              >
-                <div
-                  className={`w-[1.375rem] h-[0.125rem] bg-white transition-all duration-300 ease-in ${
-                    isHamburgerMenu ? "absolute rotate-45" : ""
-                  }`}
-                />
-                <div
-                  className={`${
-                    isHamburgerMenu ? "hidden" : ""
-                  } w-[.75rem] h-[0.125rem] bg-white`}
-                />
-                <div
-                  className={`w-[1.375rem] h-[0.125rem] bg-white transition-all duration-300 ease-in ${
-                    isHamburgerMenu ? "absolute -rotate-45" : ""
-                  }`}
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
-    </React.Fragment>
-  );
+            )}
+        </React.Fragment>
+    );
 }
