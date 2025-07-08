778b00cdeaf166930e35a91614933c45e7d4b9e1
sruthi chandrasekar <sruthiamutharani@gmail.com>
2025-07-08 11:49:31 +0530

    agent folder structuref

--- app/agent/page.tsx
+++ app/agent/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import Navbar from '../../components/layout/Navbar';
+import Navbar from '../components/layout/Navbar';
 import LenisProvider from '@/app/LenisProvider';
+import AgentTitle from '../components/layout/Agent/AgentTitle';
 // import { caseStudyData } from '@/lib/custom_data';
 
 type Props = {
@@ -13,9 +14,9 @@ export default async function Page({ params }: Props) {
 
   return (
       <LenisProvider>
-        <div className='w-screen flex items-center justify-center flex-col bg-primary-text'>
+        <div className='w-screen h-screen flex items-center justify-center flex-col bg-[#080808]'>
           <Navbar />
-          
+          <AgentTitle />
         </div>
       </LenisProvider>
   );
