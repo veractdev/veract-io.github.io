b40bd9a2235070a12a0ff0aea63fdb23194df87e
sruthi chandrasekar <sruthiamutharani@gmail.com>
2025-07-09 19:22:40 +0530

    agentic layer responsiveness

--- app/components/layout/Agent/AgenticLayer.tsx
+++ app/components/layout/Agent/AgenticLayer.tsx
@@ -18,7 +18,7 @@ export default function AgenticLayer() {
           <div className="relative lg:w-[401px] lg:h-[401px] md:w-[13.66906rem] md:h-[14.25881rem] w-[218px] h-[218px] rounded-full flex items-center justify-center overflow-visible">
             <div className="absolute agenticLayerBase lg:w-[401px] lg:h-[401px] md:w-[13.66906rem] md:h-[14.25881rem] w-[218px] h-[218px] rounded-full z-0"></div>
             <div
-              className="absolute z-20 lg:top-[51px] lg:left-[51px] md:top-[1.77rem] md-[1.87rem] top-[1.25rem] lg:w-[394px] lg:h-[394px] md:w-[13.66906rem] md:h-[14.25881rem] w-[218px] h-[218px] pointer-events-none md:animate-agentic-layer"
+              className="absolute z-20 lg:top-[51px] lg:left-[51px] md:top-[1.77rem] md-[1.87rem] top-[1.25rem] lg:w-[394px] lg:h-[394px] md:w-[13.66906rem] md:h-[14.25881rem] w-[218px] h-[218px] pointer-events-none animate-agentic-layer"
               style={{
                 filter: "url(#filter0_g_1_1075)",
                 mask: "url(#intersectionMask)",
@@ -32,7 +32,7 @@ export default function AgenticLayer() {
               />
             </div>
 
-            <div className="absolute z-30 text-[#FAFAFA] syneFont lg:text-[60px] md:text-[30px] text-[26px] text-center font-semibold lg:leading-[60px] leading-[2.03863rem] lg:top-[176px] lg:left-[141px] lg:w-full w-[6.875rem] animate-agentic-layer-opacity">
+            <div className="absolute z-30 text-[#FAFAFA] syneFont lg:text-[60px] md:text-[30px] text-[26px] text-center font-semibold lg:leading-[60px] leading-[2.03863rem] lg:top-[176px] lg:left-[141px] md:top-[6.5rem] md:left-[4.5rem] top-[6rem] lg:w-full md:w-[7.935rem] w-[6.875rem] animate-agentic-layer-opacity">
               Ai Agent Layer
             </div>
 
@@ -85,7 +85,7 @@ export default function AgenticLayer() {
               </mask>
             </svg>
           </div>
-          <div className="flex lg:top-[51px] lg:left-[54px] relative md:animate-agentic-layer-opacity">
+          <div className="flex lg:top-[51px] lg:left-[54px] relative animate-agentic-layer-opacity">
             <img
               src="/Images/agent/agenticLayerVeins.svg"
               alt="agenticLayer_2"
@@ -101,14 +101,14 @@ export default function AgenticLayer() {
             <img
               src="/Images/agent/bottom-blur.svg"
               alt="blurry effect"
-              className="absolute bottom-[-102px] left-[-54px] z-10 md:animate-agentic-layer-opacity md:opacity-100 opacity-0"
+              className="absolute md:bottom-[-85px] bottom-[-102px] left-[-54px] z-10 md:animate-agentic-layer-opacity md:opacity-100 opacity-0"
             />
 
             <div className="relative z-20 lg:top-[51px] lg:left-[54px] flex flex-col lg:w-[411px] md:w-[14.25rem]  w-[289.26px] h-max lg:pt-[57px] lg:pb-[50px] pb-[2.22rem] pt-[2.51rem] rounded-[30px] bg-[#0D0D0D] animate-agentic-layer-shadow"
             style={isMobile() ? {boxShadow: '0px 0px 12px 1px #008FFF inset'} : {}}
             >
-              <div className="flex flex-col lg:pl-[41px] pl-[1.8rem] lg:pr-[68px] pr-[2.99rem] lg:gap-[25px] gap-[1.1rem] lg:pb-[101px] pb-[4.54rem]">
-                <div className="flex flex-row lg:gap-[13px] gap-[0.57181rem]">
+              <div className="flex flex-col lg:pl-[41px] pl-[1.8rem] lg:pr-[68px] pr-[2.99rem] lg:gap-[25px] md:gap-[0.87rem]  gap-[1.1rem] lg:pb-[101px] md:pb-[1.69rem] pb-[4.54rem]">
+                <div className="flex flex-row lg:gap-[13px] md:gap-[0.45rem] gap-[0.57181rem]">
                   <div>
                     <img
                       src="/Images/agent/ai-magic.svg"
@@ -120,7 +120,7 @@ export default function AgenticLayer() {
                     Automate repetitive tasks
                   </div>
                 </div>
-                <div className="flex flex-row lg:gap-[13px] gap-[0.57181rem]">
+                <div className="flex flex-row lg:gap-[13px] md:gap-[0.45rem] gap-[0.57181rem]">
                   <div>
                     <img
                       src="/Images/agent/ai-magic.svg"
@@ -132,7 +132,7 @@ export default function AgenticLayer() {
                     Unify Disconnected Tools
                   </div>
                 </div>
-                <div className="flex flex-row lg:gap-[13px] gap-[0.57181rem]">
+                <div className="flex flex-row lg:gap-[13px] md:gap-[0.45rem] gap-[0.57181rem]">
                   <div>
                     <img
                       src="/Images/agent/ai-magic.svg"
@@ -144,7 +144,7 @@ export default function AgenticLayer() {
                     Accelerate Decision Making
                   </div>
                 </div>
-                <div className="flex flex-row lg:gap-[13px] gap-[0.57181rem]">
+                <div className="flex flex-row lg:gap-[13px] md:gap-[0.45rem] gap-[0.57181rem]">
                   <div>
                     <img
                       src="/Images/agent/ai-magic.svg"
@@ -156,7 +156,7 @@ export default function AgenticLayer() {
                     Boost Team Efficiency
                   </div>
                 </div>
-                <div className="flex flex-row lg:gap-[13px] gap-[0.57181rem]">
+                <div className="flex flex-row lg:gap-[13px] md:gap-[0.45rem] gap-[0.57181rem]">
                   <div>
                     <img
                       src="/Images/agent/ai-magic.svg"
@@ -169,11 +169,11 @@ export default function AgenticLayer() {
                   </div>
                 </div>
               </div>
-              <div className="text-white text-center dmSansFont lg:text-[20px] text-[0.875rem] font-normal leading-normal pl-[40px] pr-[52px] pb-[45px]">
+              <div className="text-white text-center dmSansFont lg:text-[20px] text-[0.875rem] font-normal leading-normal md:pl-[1.17rem] md:pr-[1.21rem] pl-[40px] pr-[52px] pb-[45px]">
                 Your tools stay the same. The way you work changes forever!
               </div>
-              <div className="lg:px-[58px] px-[2.55rem]">
-                <div className="flex lg:w-[295px] w-[12.97625rem] lg:px-[14px] lg:py-[6px] px-[0.62rem] py-[0.26rem] justify-center items-center gap-[10px] bg-[#4285F4] lg:rounded-[10px] rounded-[0.43988rem] text-center dmSansFont lg:text-[20px] md:text-[0.69388rem] text-[0.875rem] font-normal leading-normal text-white">
+              <div className="lg:px-[58px] md:px-[1.99rem] px-[2.55rem]">
+                <div className="flex lg:px-[14px] lg:py-[6px] md:py-[0.21rem] md:px-[0.49rem] px-[0.62rem] py-[0.26rem] justify-center items-center gap-[10px] bg-[#4285F4] lg:rounded-[10px] rounded-[0.43988rem] text-center dmSansFont lg:text-[20px] md:text-[0.69388rem] text-[0.875rem] font-normal leading-normal text-white">
                   Supercharge Your Tools Now
                 </div>
               </div>
