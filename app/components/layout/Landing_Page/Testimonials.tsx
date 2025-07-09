55035d140961b5d48ebb2cf2c7767d84da28a916
veract dev <veractdev@gmail.com>
2025-07-09 20:10:31 +0530

    testimonial active and inactive state without transition

--- app/components/layout/Landing_Page/Testimonials.tsx
+++ app/components/layout/Landing_Page/Testimonials.tsx
@@ -18,23 +18,27 @@ export default function Testimonials() {
       <div className='flex flex-row items-center justify-center gap-[1.125rem]'>
         <motion.div
           layout
-          // onMouseEnter={() => setActive(true)}
-          // onMouseLeave={() => setActive(false)}
+          onMouseEnter={() => setActive(true)}
+          onMouseLeave={() => setActive(false)}
           animate={{ width: isActive ? '19.625rem' : '5.125rem' }}
           transition={{ duration: 0.3, ease: 'easeInOut' }}
           className='h-[27.5rem] rounded-[2.5rem] border-[0.25rem] border-[#4285F4] shadow-[0px_0px_13px_0px_#4287F5] flex flex-col items-center justify-start gap-[2.375rem] py-[1.625rem] relative'
         >
-          {/* <img src="/Images/LandingPage/Testimonial/quotes.png" alt="testimonial quote" className='w-[1.938rem] h-[1.438rem] absolute top-[6.688rem] left-[0.875rem]' /> */}
-          <div className='w-full flex flex-row items-center justify-center gap-[0.625rem] pl-[1.313rem]'>
-            <img src="/Images/LandingPage/Testimonial/testimonial_profile.png" alt="profile image" className='w-[3.688rem] h-[3.688rem] rounded-full' />
-            <div className='w-full flex flex-col items-start justify-center gap-[0.625rem]'>
-              <div className='interFont text-[1.25rem] text-[#E3E3E3] font-semibold leading-[1.2em]'>Hare Priya Rajendran</div>
-              <div className='interFont text-[1.125rem] text-[#E3E3E3] font-normal leading-[1.2em]'>Geeraj Business Solution</div>
+          <img src="/Images/LandingPage/Testimonial/quotes.png" alt="testimonial quote" className={`${isActive ? '' : 'hidden'} w-[1.938rem] h-[1.438rem] absolute top-[6.688rem] left-[0.875rem]`} />
+          <div className={`w-full flex flex-row items-center justify-center gap-[0.625rem] ${isActive ? 'pl-[1.313rem]' : ''}`}>
+            <img
+              src="/Images/LandingPage/Testimonial/testimonial_profile.png"
+              alt="profile image"
+              className={`${isActive ? '' : 'absolute bottom-[0.75rem] left-1/2 transform -translate-x-1/2'} w-[3.688rem] h-[3.688rem] rounded-full`}
+            />
+            <div className={`w-full flex flex-col ${isActive ? 'items-start' : 'items-center'} justify-center gap-[0.625rem]`}>
+              <div className={`${isActive ? '' : 'absolute bottom-[12.25rem] transform -rotate-90 w-max'} interFont text-[1.25rem] text-[#E3E3E3] font-semibold leading-[1.2em]`}>Hare Priya Rajendran</div>
+              <div className={`${isActive ? 'block' : 'hidden'} interFont text-[1.125rem] text-[#E3E3E3] font-normal leading-[1.2em]`}>Geeraj Business Solution</div>
             </div>
           </div>
-          {/* <div className='px-[2.875rem_1.375rem] interFont text-[1rem] text-[#E3E3E3] font-normal leading-[1.3em]'>
+          <div className={`${isActive ? '' : 'hidden'} px-[2.875rem_1.375rem] interFont text-[1rem] text-[#E3E3E3] font-normal leading-[1.3em]`}>
             I am amazed the way Veract took the initial discussion of our HRMS product MyTrackie. They swiftly got into the details and educated me wherever required. Really thrilled to work with Veract for the software upgrades of our product. I would certainly recommend Veract for product development and technical consulting.
-          </div> */}
+          </div>
         </motion.div>
       </div>
     </div>
