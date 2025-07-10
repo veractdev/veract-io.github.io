21b1f09c13487b6b7f1675b500f44ee1a5a98a83
veract dev <veractdev@gmail.com>
2025-07-10 11:35:37 +0530

    testimonial rework tab view

--- app/components/layout/Landing_Page/Testimonials.tsx
+++ app/components/layout/Landing_Page/Testimonials.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { LandingPageData } from '@/lib/custom_data';
-import { isMobile } from '@/lib/utils';
+import { isMobile, isTablet } from '@/lib/utils';
 import { motion } from 'framer-motion';
 import React, { useEffect, useState } from 'react';
 
@@ -27,14 +27,14 @@ export default function Testimonials() {
   return (
     loader && (
       <div className="w-full h-max bg-[#0D0D0D] text-white flex items-center justify-center relative z-20">
-        <div className="flex flex-col md:flex-row w-[75rem] gap-[3.125rem] md:gap-[2.75rem] h-full px-[1.5rem] md:px-[6.25rem] py-[3.125rem] items-center justify-between">
-          <div className="w-full flex flex-col md:flex-row items-center justify-center relative">
+        <div className="flex flex-col lg:flex-row w-[75rem] gap-[3.125rem] lg:gap-[2.75rem] h-full px-[1.5rem] lg:px-[6.25rem] py-[3.125rem] items-center justify-between">
+          <div className="w-full flex flex-col lg:flex-row items-center justify-center relative">
             <img
-              className="absolute top-[-3.4rem] left-1/2 transform -translate-x-1/2 md:left-[-3rem] md:-translate-x-0 w-[6rem] md:w-[7.438rem] h-[6rem] md:h-[6.938rem] object-contain z-0"
+              className="absolute top-[-3.4rem] left-1/2 transform -translate-x-1/2 lg:left-[-3rem] lg:-translate-x-0 w-[6rem] lg:w-[7.438rem] h-[6rem] lg:h-[6.938rem] object-contain z-0"
               src="/Images/LandingPage/Testimonial/TestimonialDots.png"
               alt="Testimonial_Quotes"
             />
-            <div className="w-max relative z-10 syneFont text-[3.125rem] md:text-[5rem] leading-[1.2em] font-semibold flex flex-col gap-[0.625rem]">
+            <div className="w-max relative z-10 syneFont text-[3.125rem] md:text-[4rem] lg:text-[5rem] leading-[1.2em] font-semibold flex flex-col gap-[0.625rem]">
               <span
                 style={{
                   background: 'linear-gradient(355deg, rgb(156, 156, 161) 48%, rgba(250, 250, 250, 0) 100%)',
@@ -65,7 +65,7 @@ export default function Testimonials() {
             </div>
           </div>
 
-          <div className='w-full h-max flex flex-col items-center justify-center md:flex-row gap-[1.125rem]'>
+          <div className='w-full md:w-[38.25rem] lg:w-full h-max flex flex-col items-center justify-center lg:flex-row gap-[1.125rem] md:gap-[1.25rem]'>
             {LandingPageData.testimonials.testimonial_list.map((t, index) => {
               // Expanded card
               if (activeIndex === index) {
@@ -73,10 +73,10 @@ export default function Testimonials() {
                   <motion.div
                     key={index}
                     initial={{ width: "5.125rem" }}
-                    animate={{ width: "20.125rem" }}
+                    animate={{ width: isTablet() ? '38.25rem' : "20.125rem" }}
                     transition={{ duration: 0.7, ease: "easeInOut" }}
                     style={{ boxShadow: 'rgb(66, 135, 245) 0rem 0rem 0.813rem 0rem' }}
-                    className="h-[27.5rem] rounded-[2.5rem] px-[1.563rem] py-[1.675rem] text-white border-[0.25rem] border-[#4285F4] overflow-hidden"
+                    className="md:h-[19.938rem] h-[27.5rem] rounded-[2.5rem] px-[1.563rem] py-[1.675rem] text-white border-[0.25rem] border-[#4285F4] overflow-hidden"
                   >
                     {/* Full Card Content */}
                     <div
@@ -87,7 +87,7 @@ export default function Testimonials() {
                         animate={{ x: 0, y: 0, opacity: 1 }}
                         transition={{ duration: 0.5, ease: "easeInOut" }}
 
-                        className="w-[2.75rem] h-[2.75rem] md:w-[3.688rem] md:h-[3.688rem] mx-0 my-0 md:mx-[0.75rem] md:my-[0.50rem] rounded-full" src={t.image} alt="Testimonial_Client" />
+                        className="w-[2.75rem] h-[2.75rem] lg:w-[3.688rem] lg:h-[3.688rem] mx-0 my-0 lg:mx-[0.75rem] lg:my-[0.50rem] rounded-full" src={t.image} alt="Testimonial_Client" />
 
                       <motion.div
                         initial={{ x: 100, y: -50, opacity: 0 }}
@@ -131,19 +131,19 @@ export default function Testimonials() {
                   <motion.button
                     key={index}
                     initial={{ width: "20.125rem" }}
-                    animate={{ width: isMobile() ? "100%" : "5.125rem" }}
+                    animate={{ width: isMobile() || isTablet() ? "100%" : "5.125rem" }}
                     transition={{ duration: 0.7, ease: "easeInOut" }}
                     onMouseEnter={() => handleSetActiveIndex(index)}
-                    className="relative h-max md:h-[27.5rem] flex-shrink-0 cursor-pointer flex flex-row md:flex-col items-center justify-start md:justify-end rounded-[2.5rem] border-[0.25rem] border-[#4285F4] overflow-hidden"
+                    className="relative h-max lg:h-[27.5rem] flex-shrink-0 cursor-pointer flex flex-row lg:flex-col items-center justify-start lg:justify-end rounded-[2.5rem] border-[0.25rem] border-[#4285F4] overflow-hidden"
                   >
                     {visibleMiniIndex === index ? (
                       // Mini card content (after 3s)
-                      <div className='p-[0.625rem] md:p-0 flex flex-row items-center justify-center gap-[2.063rem]'>
+                      <div className='p-[0.625rem] lg:p-0 flex flex-row items-center justify-center gap-[2.063rem]'>
                         <motion.img
                           initial={{ opacity: 0 }}
                           animate={{ opacity: 1 }}
                           transition={{ duration: 0.5, ease: "easeInOut" }}
-                          className="w-[2.75rem] h-[2.75rem] md:w-[3.688rem] md:h-[3.688rem] mx-0 my-0 md:mx-[0.75rem] md:my-[0.50rem] rounded-full"
+                          className="w-[2.75rem] h-[2.75rem] lg:w-[3.688rem] lg:h-[3.688rem] mx-0 my-0 lg:mx-[0.75rem] lg:my-[0.50rem] rounded-full"
                           src={t.image}
                           alt="Testimonial_Client"
                         />
@@ -151,7 +151,7 @@ export default function Testimonials() {
                           initial={{ opacity: 0 }}
                           animate={{ opacity: 1 }}
                           transition={{ duration: 0.5, ease: "easeInOut" }}
-                          className="static md:absolute left-2/3 bottom-[6.5rem]  transform  origin-bottom-left rotate-0 md:rotate-[-90deg] interFont font-semibold text-[0.938rem] leading-[1.2em] md:text-[1.25rem] text-white whitespace-nowrap text-center">
+                          className="static lg:absolute left-2/3 bottom-[6.5rem]  transform  origin-bottom-left rotate-0 lg:rotate-[-90deg] interFont font-semibold text-[0.938rem] leading-[1.2em] lg:text-[1.25rem] text-white whitespace-nowrap text-center">
                           {t.name}
                         </motion.div>
                       </div>
@@ -161,7 +161,7 @@ export default function Testimonials() {
                         key={index}
                         transition={{ duration: 0.4, ease: "easeInOut" }}
                         style={{ boxShadow: 'rgb(66, 135, 245) 0rem 0rem 0.813rem 0rem' }}
-                        className="h-max md:h-[27.5rem] rounded-[2.5rem] px-[1.563rem] py-[1.675rem] text-white overflow-hidden"
+                        className="h-max lg:h-[27.5rem] rounded-[2.5rem] px-[1.563rem] py-[1.675rem] text-white overflow-hidden"
                       >
                         <div
                           className="flex items-center gap-[0.625rem] pb-[2.438rem]">
@@ -210,14 +210,14 @@ export default function Testimonials() {
                 <button
                   key={index}
                   onMouseEnter={() => handleSetActiveIndex(index)}
-                  className="relative w-full p-[0.625rem] gap-[2.063rem] md:gap-0 md:p-0 md:w-[5.125rem] h-max md:h-[27.5rem] flex-shrink-0 cursor-pointer flex flex-row md:flex-col items-center justify-start md:justify-end rounded-[2.5rem] border-[0.25rem] border-[#4285F4]"
+                  className="relative w-full p-[0.625rem] gap-[2.063rem] lg:gap-0 lg:p-0 lg:w-[5.125rem] h-max lg:h-[27.5rem] flex-shrink-0 cursor-pointer flex flex-row lg:flex-col items-center justify-start lg:justify-end rounded-[2.5rem] border-[0.25rem] border-[#4285F4]"
                 >
                   <img
-                    className="w-[2.75rem] h-[2.75rem] md:w-[3.688rem] md:h-[3.688rem] mx-0 my-0 md:mx-[0.75rem] md:my-[0.50rem] rounded-full"
+                    className="w-[2.75rem] h-[2.75rem] lg:w-[3.688rem] lg:h-[3.688rem] mx-0 my-0 lg:mx-[0.75rem] lg:my-[0.50rem] rounded-full"
                     src={t.image}
                     alt="Testimonial_Client"
                   />
-                  <div className="static md:absolute left-2/3 bottom-[6.5rem]  transform  origin-bottom-left rotate-0 md:rotate-[-90deg] interFont font-semibold text-[0.938rem] leading-[1.2em] md:text-[1.25rem] text-[#E3E3E3] whitespace-nowrap text-center">
+                  <div className="static lg:absolute left-2/3 bottom-[6.5rem]  transform  origin-bottom-left rotate-0 lg:rotate-[-90deg] interFont font-semibold text-[0.938rem] leading-[1.2em] lg:text-[1.25rem] text-[#E3E3E3] whitespace-nowrap text-center">
                     {t.name}
                   </div>
                 </button>
