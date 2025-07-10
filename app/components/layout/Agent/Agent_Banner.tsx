72adc92eb5322ce27cfdcb5958758c3f713a152b
sruthi chandrasekar <sruthiamutharani@gmail.com>
2025-07-10 14:08:17 +0530

    agent banner px to rem

--- app/components/layout/Agent/Agent_Banner.tsx
+++ app/components/layout/Agent/Agent_Banner.tsx
@@ -11,10 +11,10 @@ export default function AgentBanner() {
 
   return (
     loaded && (
-      <div className="w-full h-max overflow-scroll flex flex-col items-center justify-start bg-[#0d0d0d] pt-0 lg:pt-[107px] md:pt-[107px] pb-[100px]">
+      <div className="w-full h-max overflow-scroll flex flex-col items-center justify-start bg-[#0d0d0d] pt-0 lg:pt-[6.688rem] md:pt-[6.688rem] pb-[6.25rem]">
         <div className="flex flex-col items-center justify-center relative">
           <div
-            className="w-[400px] md:w-[800px] lg:w-[800px] h-[300px] md:h-[700px] lg:h-[700px] absolute z-0 left-1/2 top-0 translate-y-1/2 -translate-x-1/2 md:-translate-x-1/2 md:translate-y-0 lg:-translate-x-1/2 lg:translate-y-0 animate-spin [animation-duration:6s] [animation-timing-function:linear] blur-[12px] md:blur-none lg:blur-none"
+            className="w-[25rem] md:w-[50rem] lg:w-[50rem] h-[18.75rem] md:h-[43.75rem] lg:h-[43.75rem] absolute z-0 left-1/2 top-0 translate-y-1/2 -translate-x-1/2 md:-translate-x-1/2 md:translate-y-0 lg:-translate-x-1/2 lg:translate-y-0 animate-spin [animation-duration:6s] [animation-timing-function:linear] blur-[12px] md:blur-none lg:blur-none"
             style={{
               backgroundImage: `url(${"/Images/agent/radar_component_banner.png"})`,
               backgroundRepeat: "no-repeat",
@@ -22,25 +22,25 @@ export default function AgentBanner() {
               backgroundSize: "contain",
             }}
           ></div>
-          <div className="w-[400px] md:w-[800px] lg:w-[800px] h-[300px] md:h-[500px] lg:h-[500px] absolute bottom-0 overflow-hidden pointer-events-none">
+          <div className="w-[25rem] md:w-[50rem] lg:w-[50rem] h-[18.75rem] md:h-[31.25rem] lg:h-[31.25rem] absolute bottom-0 overflow-hidden pointer-events-none">
             <div className="absolute inset-0 bg-[#0d0d0d]/50 backdrop-blur-md [mask-image:linear-gradient(to_top,black_85%,transparent_100%)]"></div>
           </div>
-          <div className="text-white w-[100%] flex flex-col items-center justify-center mt-[107px] z-5 pl-[18px] pr-[18px] md:pl-0 md:pr-0 lg:pl-0 lg:pr-0">
-            <div className="p-[2px] rounded-[26px] bg-[linear-gradient(105deg,_rgb(41,52,255)_-2%,_rgba(36,65,212,0)_50%)]">
-              <div className="pl-[16px] pr-[16px] pt-[8px] pb-[8px] bg-black rounded-[26px] text-[#8AA5FF] font-semibold">
+          <div className="text-white w-[100%] flex flex-col items-center justify-center mt-[6.688rem] z-5 pl-[1.125rem] pr-[1.125rem] md:pl-0 md:pr-0 lg:pl-0 lg:pr-0">
+            <div className="p-[2px] rounded-[1.625rem] bg-[linear-gradient(105deg,_rgb(41,52,255)_-2%,_rgba(36,65,212,0)_50%)]">
+              <div className="pl-[1rem] pr-[1rem] pt-[0.5rem] pb-[0.5rem] bg-black rounded-[1.625rem] text-[#8AA5FF] font-semibold">
                 {AgentPageData.banner.about}
               </div>
             </div>
-            <div className="text-[26px] md:text-[36px] lg:text-[50px] text-wrap w-[100%] md:w-[60%] lg:w-[45%] [@media(min-width:2000px)]:w-[25%] text-center font-medium tracking-[-0.01em] leading-[1.2em] md:mt-[12px] lg:mt-[12px] mt-[33px]">
+            <div className="text-[1.625rem] md:text-[2.25rem] lg:text-[3.125rem] text-wrap w-[100%] md:w-[60%] lg:w-[45%] text-center font-medium tracking-[-0.01em] leading-[1.2em] md:mt-[0.75rem] lg:mt-[0.75rem] mt-[2.063rem]">
               {AgentPageData.banner.title}
             </div>
-            <div className="text-[14px] md:text-[16px] lg:text-[16px] text-wrap w-[100%] md:w-[50%] lg:w-[30%] [@media(min-width:2000px)]:w-[25%] text-center text-white/70 font-normal tracking-[-0.02em] leading-[1.6em] md:mt-[12px] lg:mt-[12px] mt-[33px]">
+            <div className="text-[0.875rem] md:text-[1rem] lg:text-[1rem] text-wrap w-[100%] md:w-[50%] lg:w-[30%] text-center text-white/70 font-normal tracking-[-0.02em] leading-[1.6em] md:mt-[0.75rem] lg:mt-[0.75rem] mt-[2.063rem]">
               {AgentPageData.banner.subtitle}
             </div>
-            <div className="flex items-center justify-center md:mt-[14px] lg:mt-[14px] mt-[34px] gap-[15px]">
-              <div className="relative overflow-hidden w-[156px] md:w-[170px] lg:[170px] h-[36px] text-nowrap text-white rounded-[6px] cursor-pointer flex items-center justify-center bg-[#4285F4] group">
+            <div className="flex items-center justify-center md:mt-[0.875rem] lg:mt-[0.875rem] mt-[2.125rem] gap-[0.938rem]">
+              <div className="relative overflow-hidden w-[9.75rem] md:w-[10.625rem] lg:[10.625rem] h-[2.25rem] text-nowrap text-white rounded-[6px] cursor-pointer flex items-center justify-center bg-[#4285F4] group">
                 <div className="absolute inset-0 transition-transform duration-500 ease-in-out group-hover:-translate-y-full">
-                  <div className="flex items-center justify-center h-[36px] w-full font-medium text-[14px] md:text-[14px] lg:text-[16px]">
+                  <div className="flex items-center justify-center h-[2.25rem] w-full font-medium text-[0.875rem] md:text-[0.875rem] lg:text-[1rem]">
                     {AgentPageData.banner.book_demo}
                     <img
                       src={LandingPageData.banner.get_in_touch_arrow}
@@ -48,7 +48,7 @@ export default function AgentBanner() {
                       className="w-[1rem] h-[1rem] object-contain ml-[0.25rem]"
                     />
                   </div>
-                  <div className="flex items-center justify-center h-[36px] w-full font-medium">
+                  <div className="flex items-center justify-center h-[2.25rem] w-full font-medium">
                     {AgentPageData.banner.book_demo}
                     <img
                       src={LandingPageData.banner.get_in_touch_arrow}
@@ -58,32 +58,32 @@ export default function AgentBanner() {
                   </div>
                 </div>
               </div>
-              <div className="relative overflow-hidden w-[156px] md:w-[170px] lg:[170px] h-[36px] text-nowrap text-white rounded-[6px] cursor-pointer flex items-center justify-center bg-[#0d0d0d]/80 group border border-[#FFFFFF]/10">
+              <div className="relative overflow-hidden w-[9.75rem] md:w-[10.625rem] lg:[10.625rem] h-[2.25rem] text-nowrap text-white rounded-[6px] cursor-pointer flex items-center justify-center bg-[#0d0d0d]/80 group border border-[#FFFFFF]/10">
                 <div className="absolute inset-0 transition-transform duration-500 ease-in-out group-hover:-translate-y-full">
-                  <div className="flex items-center justify-center h-[36px] w-full font-medium mt-[-0.15rem] text-[14px] md:text-[14px] lg:text-[16px]">
+                  <div className="flex items-center justify-center h-[2.25rem] w-full font-medium mt-[-0.15rem] text-[0.875rem] md:text-[0.875rem] lg:text-[1rem]">
                     {AgentPageData.banner.view_case_study}
                   </div>
-                  <div className="flex items-center justify-center h-[36px] w-full font-medium mt-[-0.1rem]">
+                  <div className="flex items-center justify-center h-[2.25rem] w-full font-medium mt-[-0.1rem]">
                     {AgentPageData.banner.view_case_study}
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          <div className="w-[100%] md:w-[90%] lg:w-[80%] grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-[24px] pl-[18px] md:pl-[40px] md:pr-[40px] pr-[18px] lg:pl-[40px] lg:pr-[40px] mt-[46px] z-5">
+          <div className="w-[100%] md:w-[90%] lg:w-[80%] grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-[1.5rem] pl-[1.125rem] md:pl-[2.5rem] md:pr-[2.5rem] pr-[1.125rem] lg:pl-[2.5rem] lg:pr-[2.5rem] mt-[2.875rem] z-5">
             {AgentPageData.banner.banner_features.map((feature, index) => (
               <div
                 key={index}
                 className={`interFont bg-[radial-gradient(96%_96%_at_50%_7.5%,_#121426_0%,_#000000_100%)] 
-                          w-full rounded-[16px] p-[32px] border-[#222222] 
+                          w-full rounded-[1rem] p-[2rem] border-[#222222] 
                           shadow-[0px_1px_2px_0px_#000000] border border-t-[#8AA5FF]/70 
                           ${index === 2 ? "sm:col-span-2 lg:col-span-1" : ""}`}
               >
                 <img src={feature.features_icon} alt={feature.features_title} />
-                <div className="text-[22px] mt-[24px] tracking-[-0.02em] leading-[28px] text-white font-bold">
+                <div className="text-[1.375rem] mt-[1.5rem] tracking-[-0.02em] leading-[1.75rem] text-white font-bold">
                   {feature.features_title}
                 </div>
-                <div className="text-[16px] mt-[16px] tracking-[-0.02em] leading-[1.6em] text-white/70 font-medium">
+                <div className="text-[1rem] mt-[1rem] tracking-[-0.02em] leading-[1.6em] text-white/70 font-medium">
                   {feature.features_subtitle}
                 </div>
               </div>
