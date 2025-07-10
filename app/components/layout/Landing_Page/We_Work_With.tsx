6cd739b7b79e93da13b81da3c12510d15cda336a
shiva <veractdev@gmail.com>
2025-07-10 16:33:58 +0530

    image for we work with us

--- app/components/layout/Landing_Page/We_Work_With.tsx
+++ app/components/layout/Landing_Page/We_Work_With.tsx
@@ -34,13 +34,13 @@ export default function We_Work_With() {
             {LandingPageData.we_work_with.carouselData.map((brand, index) => (
               <div
                 key={index}
-                className="chakraPetchFont w-[18.75rem] h-[7.875rem] lg:h-[12.5rem] md:h-[12.5rem] flex items-center justify-center text-white text-[2.25rem] font-medium"
+                className="relative chakraPetchFont w-[18.75rem] h-[7.875rem] lg:h-[12.5rem] md:h-[12.5rem] flex items-center justify-center text-white text-[2.25rem] font-medium"
                 style={{
-                  backgroundImage: `url(${brand.image})`,
                   marginLeft: index !== 0 ? "0.625rem" : "-0.3125rem",
                 }}
               >
-                {brand.name}
+                <img src={brand.image} className="absolute top-0 left-0 z-10 w-[18.75rem] h-[12.5rem]" />
+                <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] z-20 text-center">{brand.name}</div>
               </div>
             ))}
           </Marquee>
