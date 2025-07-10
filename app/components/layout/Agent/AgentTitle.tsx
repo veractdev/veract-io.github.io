daeddbd696464eadb04a420e9bbcf8ce2b4074c0
sruthi chandrasekar <sruthiamutharani@gmail.com>
2025-07-10 14:28:49 +0530

    px to rem conversion

--- app/components/layout/Agent/AgentTitle.tsx
+++ app/components/layout/Agent/AgentTitle.tsx
@@ -14,12 +14,12 @@ export default function AgentTitle({ title, title_description, description }: { 
           <div className="flex flex-row items-center justify-center gap-[10px] rounded-[10px] bg-[linear-gradient(181deg,rgba(0,85,254,0.08)_0.51%,rgba(153,153,153,0.10)_99.49%)] relative px-[14px] py-[6px]">
             <div className="absolute top-[2px] w-[61px] h-[1px] bg-[linear-gradient(90deg,rgba(66,133,244,0)_0%,#4285F4_50%,rgba(66,133,244,0)_100%)]"></div>
             <div className="w-[10px] h-[10px] bg-[#FAFAFA] rounded-full"></div>
-            <div className="text-white text-center dmSansFont md:text-[26px] text-[14px] font-normal leading-normal">{title}</div>
+            <div className="text-white text-center dmSansFont md:text-[1.625rem] text-[14px] font-normal leading-normal">{title}</div>
         </div>
-          <div className="text-[#FAFAFA] text-center syneFont lg:text-[54px] md:text-[50px] text-[28px] font-semibold leading-normal max-w-[1139px] md:px-0 px-[7.5px]">
+          <div className="text-[#FAFAFA] text-center syneFont lg:text-[54px] md:text-[50px] text-[1.75rem] font-semibold leading-normal max-w-[1139px] md:px-0 px-[7.5px]">
             {title_description}
           </div>
-          <div className="text-[#CECECE] text-center dmSansFont md:text-[26px] text-[18px] font-normal leading-normal lg:pb-[136px]  pb-[6.94rem] max-w-[652px]">
+          <div className="text-[#CECECE] text-center dmSansFont md:text-[1.625rem] text-[1.125rem] font-normal leading-normal lg:pb-[136px]  pb-[6.94rem] max-w-[652px]">
             {description}
           </div>
         </div>
