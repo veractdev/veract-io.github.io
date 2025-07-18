d8df228f66be06109436c8b11e87f9472717a3e3
veract dev <veractdev@gmail.com>
2025-07-18 14:47:39 +0530

    showreel over glitch

--- app/components/layout/Landing_Page/ShowReel.tsx
+++ app/components/layout/Landing_Page/ShowReel.tsx
@@ -170,7 +170,7 @@ export default function ShowReel() {
               />
             </div>
 
-            <div className='syneFont text-[1rem] text-black font-bold leading-[1.2em] uppercase opacity-0 group-hover:opacity-100 translate-y-6 group-hover:translate-y-0 transition-all duration-300'>
+            <div className='syneFont text-[1rem] text-black font-bold leading-[1.2em] uppercase opacity-100 translate-y-6 transition-all duration-300'>
               play showreel
             </div>
           </motion.div>
