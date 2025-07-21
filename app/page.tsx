fb85a5d3d60eca8544a25e08b41682db25151c84
veract dev <veractdev@gmail.com>
2025-07-21 16:04:55 +0530

    page structure change

--- app/page.tsx
+++ app/page.tsx
@@ -43,16 +43,16 @@ export default function Page() {
               href={image}
             />
           ))}
-          {/* <Navbar />
+          <Navbar />
           <Banner />
           <ShowReel />
           <About_Us />
           <Advantages />
           <Services />
           <We_Work_With />
-          <Testimonials /> */}
+          <Testimonials />
           <FAQ />
-          {/* <Footer /> */}
+          <Footer />
         </div>
       )}
     </LenisProvider>
