9f6ce5567eaa66141d12f9d44fc07a482fd72c57
sruthi chandrasekar <sruthiamutharani@gmail.com>
2025-07-25 20:02:10 +0530

    alt for images in agent page

--- app/components/layout/Agent/AgentRobot.tsx
+++ app/components/layout/Agent/AgentRobot.tsx
@@ -19,6 +19,7 @@ export default function AgentRobot({ agent }: { agent: Agent }) {
       <React.Fragment>
         <img loading="lazy"
           src={`${baseUrl}/Images/agent/polygon.svg`}
+          alt="polygon"
           className="absolute lg:top-[85%] md:top-[90%] top-[85%] left-[50%] translate-x-[-50%] z-[1] lg:w-max md:w-[1.438rem] w-[0.938rem]"
         />
         <div className="relative z-[100] lg:w-[9.881rem] lg:h-[9.881rem] md:w-[5.516rem] md:h-[5.516rem] w-[2.76363rem] h-[2.76363rem] bg-[#1C1C1C] rounded-full flex items-center justify-center">
