3aea774190b1d3ce973bdd42ccd49621461b82bc
shiva <veractdev@gmail.com>
2025-08-01 17:39:13 +0530

    build files

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,16 +1,1 @@
-globalThis.__BUILD_MANIFEST = {
-  "pages": {
-    "/_app": []
-  },
-  "devFiles": [],
-  "ampDevFiles": [],
-  "polyfillFiles": [],
-  "lowPriorityFiles": [],
-  "rootMainFiles": [],
-  "ampFirstPages": []
-};
-globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
-"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
-,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
-
-];
+globalThis.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-92dcaa06aa8ac813.js","static/chunks/4bd1b696-299743f5624cdabe.js","static/chunks/303-603ebaa76d6ad84f.js","static/chunks/main-app-c35571c6a6ddf8eb.js"],rootMainFilesTree:{},pages:{"/_app":["static/chunks/webpack-92dcaa06aa8ac813.js","static/chunks/framework-f593a28cde54158e.js","static/chunks/main-2d003aadaf021ba6.js","static/chunks/pages/_app-92f2aae776f86b9c.js"],"/_error":["static/chunks/webpack-92dcaa06aa8ac813.js","static/chunks/framework-f593a28cde54158e.js","static/chunks/main-2d003aadaf021ba6.js","static/chunks/pages/_error-71d2b6a7b832d02a.js"]},ampFirstPages:[]},globalThis.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
