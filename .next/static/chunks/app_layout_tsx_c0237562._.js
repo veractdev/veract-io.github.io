e535ac8c77e5042d102bc20e3cb18d7858fbd593
shiva <veractdev@gmail.com>
2025-08-04 15:06:08 +0530

    build files

--- .next/static/chunks/app_layout_tsx_c0237562._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__9d126714._.css",
-  "static/chunks/_336e1cfe._.js"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/app_services_page_tsx_b384302a._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_ff4886b9._.js",
-  "static/chunks/node_modules_8c3cd6c3._.js"
-],
-    source: "dynamic"
-});
