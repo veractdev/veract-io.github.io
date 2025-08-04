fec24c154b85b72c149012f1a6fb52fb45b15149
veract dev <veractdev@gmail.com>
2025-08-04 18:17:49 +0530

    service image alignments

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -103,7 +103,7 @@ export default function Page() {
                 </div> */}
                 <span className='w-full lg:w-[350px] ' dangerouslySetInnerHTML={{ __html: service.title }} />
               </div>
-              <div className='flex flex-col md:flex-row gap-[2.31rem] md:gap-[4.38rem] lg:gap-[2.06rem] items-center md:items-start justify-center'>
+              <div className='w-full flex flex-col md:flex-row gap-[2.31rem] md:gap-[4.38rem] lg:gap-[2.06rem] items-center md:items-start justify-center'>
                 <div className='flex flex-col p-[0.625rem_1.25rem] md:p-[0.62rem_0_0_2.094rem] lg:p-0 gap-[1rem] lg:gap-[0.62rem] items-start lg:items-center justify-center'>
                   {service.service_array.map((service, index) => (
                     <div key={index} className='flex flex-row gap-[0.62rem] items-center justify-center p-[0.38rem_0.62rem]'>
@@ -112,7 +112,7 @@ export default function Page() {
                     </div>
                   ))}
                 </div>
-                <div className='flex flex-col gap-[1.38rem] items-center md:items-start justify-center py-[] md:py-[0.62rem] lg:py-0'>
+                <div className='flex flex-col gap-[1.38rem] items-center md:items-start justify-center py-0 md:py-[0.62rem] lg:py-0'>
                   {service.service_CTA.map((CTA, index) => (
                     <div
                       key={index}
