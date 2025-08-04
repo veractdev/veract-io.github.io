9890097ca7ff0de405f78b1d0ffcdecce166f8fd
shiva <veractdev@gmail.com>
2025-08-04 18:05:49 +0530

    build files

--- .next/static/chunks/app_case-studies_[slug]_page_tsx_b2f3bef0._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_18e2665a._.js",
-  "static/chunks/node_modules_7ecb2ad3._.js"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/app_not-found_tsx_b2f3bef0._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_3439562d._.js",
-  "static/chunks/_09f7a298._.js"
-],
-    source: "dynamic"
-});
