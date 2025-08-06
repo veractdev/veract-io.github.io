23cfe7fea7cb019566fc06429ca3d74c203c17a5
shiva <veractdev@gmail.com>
2025-08-06 16:32:00 +0530

    constact us validation

--- app/components/layout/ContactUs/contact_us.tsx
+++ app/components/layout/ContactUs/contact_us.tsx
@@ -69,7 +69,7 @@ export default function Contact_Us() {
             isOnlyCountryCode(mobileNumber, countryCode) ||
             mobileNumber.trim() === ""
               ? "*Required"
-              : !isValidMobileNumber(mobileNumber, countryCode)
+              : !isValidMobileNumber(mobileNumber)
               ? "Please enter a valid mobile number"
               : "",
         }));
@@ -93,12 +93,13 @@ export default function Contact_Us() {
   };
 
   // Validate mobile number (not empty)
-  const isValidMobileNumber = (mobile: string, countryCode: string) => {
+  const isValidMobileNumber = (mobile: string) => {
     try {
       const phoneNumber = parsePhoneNumberFromString("+" + mobile);
       if (!phoneNumber) return false;
       return phoneNumber.isValid();
     } catch (e) {
+      console.error("Error validating mobile number:", e);
       return false;
     }
   };
@@ -110,7 +111,7 @@ export default function Contact_Us() {
     isValidEmail(email) &&
     mobileNumber.trim() !== "" &&
     !isOnlyCountryCode(mobileNumber, countryCode) &&
-    isValidMobileNumber(mobileNumber, countryCode);
+    isValidMobileNumber(mobileNumber);
 
   // This function handles the form submission
   const handleSubmitContact = async () => {
@@ -365,7 +366,7 @@ export default function Contact_Us() {
                                 ? "*Required"
                                 : !isValidMobileNumber(
                                     phone,
-                                    `+${countryData.dialCode}`
+                                   
                                   )
                                 ? "Please enter a valid mobile number"
                                 : "",
