c277ac63a19f6c60bcdead2f219916611b88a84c
veract dev <veractdev@gmail.com>
2025-08-08 09:44:18 +0530

    testimonials spacing

--- app/components/layout/Landing_Page/Testimonials.tsx
+++ app/components/layout/Landing_Page/Testimonials.tsx
@@ -26,7 +26,7 @@ export default function Testimonials() {
 
   return (
     loader && (
-      <div className="w-full h-max bg-[#0D0D0D] text-white flex items-center justify-center relative z-20">
+      <div className="w-full h-max bg-[#0D0D0D] text-white flex items-center justify-center relative z-20 pb-[3rem] md:pb-0">
         <div className="flex flex-col lg:flex-row w-[75rem] gap-[3.125rem] lg:gap-[2.75rem] h-full px-[1.5rem] lg:px-[6.25rem] py-[0] lg:py-[12.813rem_6.125rem] md:py-[9.938rem_6.25rem] items-center justify-between">
           <div className="w-full flex flex-col lg:flex-row items-center justify-center relative">
             <img loading="lazy"
